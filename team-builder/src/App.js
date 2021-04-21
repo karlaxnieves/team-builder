c9c4cb3fdb5a7564d65ee28795fe0f06b7bdbb9a
karlaxnieves <78055815+karlaxnieves@users.noreply.github.com>
2021-04-20 21:02:18 -0700

    working on rendering memebers

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,12 +1,46 @@
 
 import './App.css';
+import Form from './Form'
+import React, { useState, useEffect } from 'react'
 
-function App() {
+
+const initialFormValue = {
+  name: '',
+  email: '',
+  role: '',
+}
+
+
+export default function App() {
+  const [member, setMember] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValue)
+
+  const updateForm = (name, value) => {
+    setFormValues({ ...formValues, [name]: value })
+  }
+
+  const submitForm = () => {
+    const newMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    }
+    if (!newMember.username || !newMember.email || !newMember.role) return
+
+    setMember([newMember, ...member]);
+    setFormValues(initialFormValue)
+  }
+
+
+
+
   return (
     <div className="App">
-
+      <Form
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
+      />
     </div>
   );
 }
-
-export default App;
